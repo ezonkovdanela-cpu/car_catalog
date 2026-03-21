736a12ba4b0ef5c2cd6b7744d382d07039c3360a
ezonkovdanela-cpu <ezonkovdanela@gmail.com>
2026-03-21 04:22:19 +0300

    feat: add 10 car records (3 base + 7 more) for analysis

---
+++ sql/insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO cars (brand, model, year, color, price) VALUES
+('Toyota', 'Camry', 2020, 'Silver', 1500000.00),
+('BMW', 'X5', 2022, 'Black', 4500000.50),
+('Lada', 'Vesta', 2021, 'White', 800000.00),
+('Ford', 'Focus', 2019, 'Blue', 950000.00),
+('Hyundai', 'Solaris', 2021, 'Red', 1100000.00),
+('Kia', 'Rio', 2022, 'White', 1150000.00),
+('Mercedes', 'E-Class', 2020, 'Black', 4200000.00),
+('Audi', 'A6', 2023, 'Silver', 5100000.00),
+('Volkswagen', 'Passat', 2021, 'Gray', 2100000.00),
+('Nissan', 'Qashqai', 2022, 'Green', 1900000.00);
